471a1bd2fa4aaefcba256680d35200b0d6dcc594
BaeJuh <106052739+BaeJuh@users.noreply.github.com>
2022-05-23 16:59:00 +0900

    Update 핸드폰 번호 가리기.js

--- Java Script/Level 1/핸드폰 번호 가리기.js
+++ Java Script/Level 1/핸드폰 번호 가리기.js
@@ -1,10 +1,11 @@
 function solution(phone_number) {
     var answer = '';
-    
+    // 휴대폰 번호 길이 - 4의 길이를 가진 *로 이루어진 문자열 생성
     for (let i=0; i<phone_number.length-4; i++) {
         answer += '*';
     }
     
+    // 앞에서 만든 문자열 + 휴대폰 번호 뒤 4자리
     answer += phone_number.slice(-4);
     
     return answer;
