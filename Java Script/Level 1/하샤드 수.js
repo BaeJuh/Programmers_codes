4ceb0ccfc899d338e7208827ac7d70f2e76e8c03
BaeJuh <106052739+BaeJuh@users.noreply.github.com>
2022-05-23 16:53:15 +0900

    Update 하샤드 수.js

--- Java Script/Level 1/하샤드 수.js
+++ Java Script/Level 1/하샤드 수.js
@@ -2,9 +2,11 @@ function solution(x) {
     let str = x + '';
     let chk = 0;
     
+    // x값을 문자열로 만든 후 각 자리의 수를 더함
     for (let i=0; i<str.length; i++) {
         chk += Number(str[i]);
     }
     
+    // 조건 확인
     return x%chk == 0;
 }
