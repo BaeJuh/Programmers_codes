9af53b863d4308e25829b49a4eba746a423aa118
BaeJuh <106052739+BaeJuh@users.noreply.github.com>
2022-05-26 16:48:48 +0900

    Update 예산.js

--- Java Script/Level 1/예산.js
+++ Java Script/Level 1/예산.js
@@ -4,6 +4,8 @@ function solution(d, budget) {
     // 최대한 많은 부서에 예산을 써야하기 때문에 오름차순으로 정렬해 예산은 낮은 부서가 앞으로 가게 함. 
     d.sort((a, b) => a - b);
     
+    // 부서를 지날 때 마다 부서의 예산 만큼 전체 예산에서 뺌
+    // 만약 부서의 예산이 남은 예산 보다 크면 종료
     for (let i=0; i<d.length; i++) {
         if (budget < d[i])
         {
