2dc766587c30b921bdbfc4f76165af30b55f2ef3
BaeJuh <106052739+BaeJuh@users.noreply.github.com>
2022-05-26 13:59:50 +0900

    Update 수박수박수박수박수박수.js

--- Java Script/Level 1/수박수박수박수박수박수.js
+++ Java Script/Level 1/수박수박수박수박수박수.js
@@ -3,12 +3,7 @@ function solution(n) {
     
     // 홀수 번째 자리에는 수, 짝수 번째 자리에 박
     for (let i=1; i<=n; i++) {
-        if (i%2==1) {
-            answer += '수';
-        }
-        else {
-            answer += '박';
-        }
+        i%2 == 1 ? answer += '수' : answer += '박';
     }
     
     return answer;
