3d6e177c9ddb4d2e8e370c13d8032a100c1e8f49
BaeJuh <106052739+BaeJuh@users.noreply.github.com>
2022-05-26 17:43:39 +0900

    Update 다트 게임.js
    설명추가

--- Java Script/Level 1/다트 게임.js
+++ Java Script/Level 1/다트 게임.js
@@ -2,13 +2,14 @@ function solution(dartResult) {
     var answer = 0;
     let score = '';
     let score_arr = [];
-    let past = 0;
     let x = 0;
     
     for (let i=0; i<dartResult.length; i++) {
+        // 계산을 위해 score에 넣어둠
         if (!isNaN(dartResult[i])) {
             score += dartResult[i];
         }
+        // s d t를 각각 확인해 맞는 계산을 해서 score_arr 배열에 넣음.
         if (dartResult[i] == 'S' || dartResult[i] == 'D' || dartResult[i] == 'T') {
             if (dartResult[i] == 'S') {
                 x = parseInt(score);
@@ -22,6 +23,8 @@ function solution(dartResult) {
             score = '';
             score_arr.push(x);
         }
+        // #은 단순 - 계산 이지만 *은 이전 스코어가 필요함(score_arr를 만든 이유)
+        // score_arr의 현재값과 이전값을 계산
         if (dartResult[i] == '*' || dartResult[i] == '#') {
             if (dartResult[i] == '*') {
                 score_arr[score_arr.length-1] = x*2;
@@ -31,9 +34,8 @@ function solution(dartResult) {
                 score_arr[score_arr.length-1] = x * (-1);
             }
         }
-        past = score_arr[score_arr.length-2];
     }
-    console.log(score_arr);
+    
     for(let i=0; i<score_arr.length; i++) {
         answer += score_arr[i];
     }
