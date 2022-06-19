c178ba04c2ae2e56dc41eb8d8a33370052203bb5
Juhwan <106052739+BaeJuh@users.noreply.github.com>
2022-06-19 14:15:24 +0900

    Update 신고 결과 받기.js

--- Java Script/Level 1/신고 결과 받기.js
+++ Java Script/Level 1/신고 결과 받기.js
@@ -4,17 +4,21 @@ function solution(id_list, report, k) {
     let reported_count = []; // [유저, 신고받은 수]
     let email_count = []; // [유저, 신고한 수]
 
+    // 중복 제거
     report = [...new Set(report)];
 
+    // [유저, 신고 횟수] 모양으로 신고당한 사람과 신고 한 사람의 신고 수를 카운팅
     for (let i=0; i<id_list.length; i++) {
         reported_count[i] = [id_list[i], 0];
         email_count[i] =  [id_list[i], 0];
     }
 
+    // 신고 내역을 분리
     for (let i=0; i<report.length; i++) {
         Chk[i] = report[i].split(' ');
     }
 
+    // 신고당한 횟수를 셈
     for (let i=0; i<Chk.length; i++) {
         for (let j=0; j<id_list.length; j++) { // reported_count, email_count의 길이는 id_list의 길이와 같음
             if (Chk[i][1] == reported_count[j][0]) {
@@ -23,6 +27,7 @@ function solution(id_list, report, k) {
         }
     }
     
+    // 신고 받은 횟수가 k회 이상이면 신고 내역에서(Chk 배열) 신고한 사람을 찾아 그 사람이 메일을 받을 횟수 + 1
     for (let i=0; i<id_list.length; i++) { 
         if (reported_count[i][1] >= k) {
             for (let j=0; j<Chk.length; j++) {
