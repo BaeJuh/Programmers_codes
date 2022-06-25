23c9091a64bcfbcef9131507953a696b7c554a8e
Juhwan <106052739+BaeJuh@users.noreply.github.com>
2022-06-25 17:37:18 +0900

    Update 행렬의 덧셈.js

--- Java Script/Level 1/행렬의 덧셈.js
+++ Java Script/Level 1/행렬의 덧셈.js
@@ -1,6 +1,7 @@
 function solution(arr1, arr2) {
     var answer = [];
-    let chk = []
+    // chk는 answer안에 결과로 들어가 배열 담당
+    let chk = [];
     
     for (let i=0; i<arr1.length; i++) {
         chk = [];
