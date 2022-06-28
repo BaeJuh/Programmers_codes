2b7aa271ff68abec5cd63f95588469d035327915
Juhwan <106052739+BaeJuh@users.noreply.github.com>
2022-06-28 16:37:40 +0900

    Update 최솟값 만들기.js
    reduce ((acc, cur, idx) => {}, 0)을 넣으면 처음값도 함수에 따라 실행

--- Java Script/level 2/최솟값 만들기.js
+++ Java Script/level 2/최솟값 만들기.js
@@ -10,3 +10,14 @@ function solution(A,B){
     
     return answer;
 }
+
+function solution(A,B){
+    var answer = 0;
+    A.sort((a, b) => a-b);
+    B.sort((a, b) => b-a);
+    
+    return A.reduce((acc, cur, idx) => {
+        acc += cur * B[idx];
+        return acc;
+    }, 0);
+}
