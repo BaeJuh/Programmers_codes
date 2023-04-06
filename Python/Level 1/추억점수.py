7d799bb96ceaea0ce33e01c7bb7bbd0ce93cd51a
Juhwan <106052739+BaeJuh@users.noreply.github.com>
2023-04-07 00:32:22 +0900

    Update 추억점수.py

--- Python/Level 1/추억점수.py
+++ Python/Level 1/추억점수.py
@@ -1,9 +1,9 @@
 def solution(name, yearning, photo):
     answer = []
     for i in range(len(photo)):
-        x=0
-        for j in range(len(name)):
-            if name[j] in photo[i]:
-                x += yearning[j]
+        x=0 #추억 점수
+        for j in range(len(name)): 
+            if name[j] in photo[i]: #사진 안에 사람이 있으면
+                x += yearning[j] #알맞는 추억 점수를 더함
         answer.append(x)
     return answer
