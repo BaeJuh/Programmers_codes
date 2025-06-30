e2a7cc0698671d320f66a587c34ce7848f36d35d
Juhwan Bae <106052739+BaeJuh@users.noreply.github.com>
2025-06-30 22:51:34 +0900

    Update 하샤드 수.js
    새로운 풀이법

--- Java Script/Level 1/하샤드 수.js
+++ Java Script/Level 1/하샤드 수.js
@@ -10,3 +10,8 @@ function solution(x) {
     // 조건 확인
     return x%chk == 0;
 }
+
+// 새로운 풀이
+function solution(x) {
+    return x%(String(x).split("").reduce((acc, cur) => (Number(acc) + Number(cur)), 0)) === 0;
+}
