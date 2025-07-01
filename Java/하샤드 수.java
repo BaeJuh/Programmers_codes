f338a8cb9eff941311743162e9d5867ceadbad79
Juhwan Bae <106052739+BaeJuh@users.noreply.github.com>
2025-07-01 21:06:53 +0900

    Update 하샤드 수.java

--- Java/하샤드 수.java
+++ Java/하샤드 수.java
@@ -4,4 +4,10 @@ class Solution {
     public boolean solution(int x) {
         return x % Arrays.stream(String.valueOf(x).split("")).mapToInt(Integer::parseInt).sum() == 0;
     }
+
+    public boolean answer(int x) {
+        int sum = 0;
+        for (int n = x; n > 0; n /= 10) sum += n % 10;
+        return x % sum == 0;
+    }
 }
