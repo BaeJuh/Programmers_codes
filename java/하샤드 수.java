aaceda96fb26f6dc4dc7602741eda71f08efd7bc
Juhwan Bae <106052739+BaeJuh@users.noreply.github.com>
2025-07-01 20:03:55 +0900

    Delete java directory

--- java/하샤드 수.java
+++
@@ -1,7 +0,0 @@
-import java.util.Arrays;
-
-class Solution {
-    public boolean solution(int x) {
-        return x % Arrays.stream(String.valueOf(x).split("")).mapToInt(Integer::parseInt).sum() == 0;
-    }
-}
