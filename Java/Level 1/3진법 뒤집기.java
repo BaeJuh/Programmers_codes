d9518c99aa9eab54c8c05bcdda5bc988b3847ce5
Juhwan Bae <106052739+BaeJuh@users.noreply.github.com>
2025-07-03 21:11:43 +0900

    Update 3진법 뒤집기.java

--- Java/Level 1/3진법 뒤집기.java
+++ Java/Level 1/3진법 뒤집기.java
@@ -12,4 +12,14 @@ public class Digit3 {
 
         return convertedNum.stream().reduce(0, (acc, cur) -> acc + cur * (int)Math.pow(digitNum, convertedNum.indexOf(cur)));
     }
+
+    public int answer(int n) {
+        int result = 0;
+        while (n > 0) {
+            result = (n % 3) + (result * 3);
+            n /= 3;
+        }
+
+        return result;
+    }
 }
