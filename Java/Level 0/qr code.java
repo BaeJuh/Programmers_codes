beb6c30d2ced0940122744d1ba011dfe47cbc67c
Juhwan Bae <106052739+BaeJuh@users.noreply.github.com>
2025-07-28 17:02:46 +0900

    Create qr code.java

---
+++ Java/Level 0/qr code.java
@@ -0,0 +1,9 @@
+public String solution(int q, int r, String code) {
+        String answer = "";
+
+        for (int i = r; i < code.length(); i += q) {
+            answer += code.substring(i, i+1);
+        }
+
+        return answer;
+    }
