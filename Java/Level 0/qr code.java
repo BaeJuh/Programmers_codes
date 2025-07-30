fb02bf8b36d23410b3c5122a2cc0027994e0ce54
Juhwan Bae <106052739+BaeJuh@users.noreply.github.com>
2025-07-30 17:45:55 +0900

    Update qr code.java

--- Java/Level 0/qr code.java
+++ Java/Level 0/qr code.java
@@ -1,3 +1,6 @@
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class QrCode {
     public String solution(int q, int r, String code) {
         String answer = "";
@@ -7,5 +10,12 @@ public class QrCode {
         }
 
         return answer;
+    }
+
+    public String Answer(int q, int r, String code) {
+        return IntStream.range(0, code.length())
+                .filter(i -> i % q == r)
+                .mapToObj(i -> String.valueOf(code.charAt(i)))
+                .collect(Collectors.joining());
     }
 }
